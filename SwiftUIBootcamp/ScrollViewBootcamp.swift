c0d202f57722b2afcef8a92713ad1cda03d56cfc
Hrutujaa21 <kamthe.hrutuja@gmail.com>
2025-02-17 22:21:13 +0530

    Modifiers Part-2

---
+++ SwiftUIBootcamp/ScrollViewBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  ScrollViewBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Hrutuja Kamthe on 17/02/25.
+//
+
+import SwiftUI
+
+struct ScrollViewBootcamp: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0..<100) { index in
+                    ScrollView(.horizontal, showsIndicators: false, content: {
+                        LazyHStack {
+                            ForEach(0..<20) { index in
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .fill(Color.white)
+                                    .frame(width: 200, height: 150)
+                                    .shadow(radius: 10)
+                                    .padding()
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollViewBootcamp()
+}
